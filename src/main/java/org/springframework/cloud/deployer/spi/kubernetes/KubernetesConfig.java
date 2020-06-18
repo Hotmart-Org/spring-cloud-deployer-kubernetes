af56b8276ab5373fba1d8f4022ab537a9d2b8698
Paulo Ribeiro <paulo.ribeiro@hotmart.com.br>
2020-06-18 11:27:51 -0300

    adding config name

--- src/main/java/org/springframework/cloud/deployer/spi/kubernetes/KubernetesConfig.java
+++ src/main/java/org/springframework/cloud/deployer/spi/kubernetes/KubernetesConfig.java
@@ -2,13 +2,19 @@ package org.springframework.cloud.deployer.spi.kubernetes;
 
 public class KubernetesConfig {
 
+	private String name;
 	private String masterUrl;
 	private String oauthToken;
 
-	public KubernetesConfig() {
-	}
+	public KubernetesConfig() {}
 
 	public KubernetesConfig(String masterUrl, String oauthToken) {
+		this.masterUrl = masterUrl;
+		this.oauthToken = oauthToken;
+	}
+
+	public KubernetesConfig(String name, String masterUrl, String oauthToken) {
+		this.name = name;
 		this.masterUrl = masterUrl;
 		this.oauthToken = oauthToken;
 	}
@@ -27,5 +33,13 @@ public class KubernetesConfig {
 
 	public void setOauthToken(String oauthToken) {
 		this.oauthToken = oauthToken;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
